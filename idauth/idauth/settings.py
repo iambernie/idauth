2aee8821f01fc9d2b27636d07f7f1a2778947de7
iambernie <iambernie@gmail.com>
2014-12-07 16:46:39 +0100

    defaults settings.py

--- idauth/idauth/settings.py
+++ idauth/idauth/settings.py
@@ -1,40 +1,36 @@
-"""
-Django settings for idauth project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
+"""Django settings for idauth project."""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'co%^3qtwlpq=ma$h!dj8oz199f^@m&9nhus)q0-svt7)$s1(-8'
+################# SECURITY #######################################
+
+with open('/home/bernie/githubs/idauth/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
+########## SECURITY: HTTPS STUFF ###############################
+#Set this to True to avoid transmitting the CSRF cookie over HTTP accidentally.
+CSRF_COOKIE_SECURE = False
 
-# Application definition
+#Set this to True to avoid transmitting the session cookie over HTTP accidentally.
+SESSION_COOKIE_SECURE = False
+
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    #'django.contrib.admin',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.messages',
     'django.contrib.staticfiles',
 )
 
@@ -49,13 +45,10 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'idauth.urls'
-
 WSGI_APPLICATION = 'idauth.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -65,19 +58,39 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/home/bernie/githubs/idauth/static/'
+STATIC_ROOT = '/home/bernie/githubs/idauth/static_root'
+STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+    'home/bernie/githubs/idauth/static/',
+)
 
-STATIC_URL = '/static/'
+####################### Templates #############################3333
+
+# List of callables that know how to import templates from various sources.
+# These are loaded by default: just here for reference
+#TEMPLATE_LOADERS = (
+#    'django.template.loaders.filesystem.Loader',
+#    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+#)
+
+#Are loaded by default?
+#TEMPLATE_CONTEXT_PROCESSORS = (
+#    "django.contrib.auth.context_processors.auth",
+#    "django.core.context_processors.debug",
+#    "django.core.context_processors.i18n",
+#    "django.core.context_processors.media",
+#    "django.core.context_processors.static",
+#    "django.core.context_processors.csrf",
+#)
+
+AUTHENTICATION_BACKENDS = ('nameless.auth.IdentityBackend',)
